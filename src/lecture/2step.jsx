9af908b82b15fb1f5e54609178241f7fcfd983a8
Lim Dong Hyeon <ilhai6@naver.com>
2024-08-26 02:16:29 +0900

    2강 업데이트 되지 않는 값 외부로 빼기

---
+++ src/lecture/2step.jsx
@@ -0,0 +1,22 @@
+const { useState } = require("react");
+
+const INFO = {
+  name: "My Component",
+  value: "Clean Code",
+};
+
+function NotUpdateValue() {
+  // 업데이트가 되지 않는 일반적인 개체
+  // 불필요한 참조가 문제가 된다.
+  // 리액트 상태로 바꾸던가 컴포넌트 외부로 내보낸다.
+  //   const INFO = {
+  //     name: "My Component",
+  //     value: "Clean Code",
+  //   };
+
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => setCount((prevCount) => prevCount);
+  const onDecrement = () => setCount((prevCount) => prevCount);
+  return <header>{INFO}</header>;
+}
